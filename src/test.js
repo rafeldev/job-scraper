2199a23229ead6070f849bda6105abaea368470b
Rafael Alvarez <rafewui@gmail.com>
2025-10-05 13:01:46 -0500

    fx

--- src/test.js
+++ src/test.js
@@ -5,3 +5,4 @@ dotenv.config();
 
 console.log(process.env.NOTION_API_KEY, 'notion api key');
 console.log(process.env.NOTION_DATABASE_ID, 'database id');
+console.log(process.env.NOTION_PARENT_PAGE_ID, 'NOTION_PARENT_PAGE_ID id');
